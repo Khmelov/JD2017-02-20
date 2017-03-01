860bb11e2d0dbd18d5c2d0a191e5048eaca4623a
anteresh <anteresh@gmail.com>
2017-03-01 12:12:32 +0300

    jd01_05 not yet done

---
+++ src/by/it/tereshko/jd01_05/Main.java
@@ -0,0 +1,10 @@
+package by.it.tereshko.jd01_05;
+
+public class Main {
+    public static void main(String[] args) {
+        double z;
+        double a = 0.94;
+        double x = 10.093;
+        System.out.println();
+    }
+}
