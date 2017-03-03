8235263a81de95a47a68c319178f8fed2e9bbbab
anteresh <anteresh@gmail..com>
2017-03-03 07:40:53 +0200

    выполнено Task 5 (КЗ1-3 вариант 14, КЗ4 вариант 8)

--- src/by/it/tereshko/jd01_05/Main.java
+++ src/by/it/tereshko/jd01_05/Main.java
@@ -2,9 +2,26 @@ package by.it.tereshko.jd01_05;
 
 public class Main {
     public static void main(String[] args) {
-        double z;
-        double a = 0.94;
-        double x = 10.093;
+//        Задача #1
+        double z = 1.5;
+        System.out.println("Task1");
+        System.out.println(Task1.optionA(z));
+        System.out.println(Task1.optionB(z));
+
+//        Задача #2
         System.out.println();
+        System.out.println("Task2");
+        System.out.println(Task2.calculate(3.24, 5.8));
+
+//        Задача #3
+        System.out.println();
+        System.out.println("Task3");
+        Task3.printTable();
+
+//        Задача #4
+        System.out.println();
+        System.out.println("Task4");
+        System.out.println(Task4.calculate(2.408));
+
     }
 }
--- src/by/it/tereshko/jd01_05/math/Task1.java
+++
@@ -1,5 +0,0 @@
-package by.it.tereshko.jd01_05.math;
-
-public class Task1 {
-
-}
