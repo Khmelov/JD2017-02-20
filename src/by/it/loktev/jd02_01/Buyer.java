0350a81d40672381fb6221aa0c87125b49a103d9
loktevalexey <loktev.alex.74@gmail.com>
2017-03-29 11:23:50 +0300

    jd02_01

---
+++ src/by/it/loktev/jd02_01/Buyer.java
@@ -0,0 +1,61 @@
+package by.it.loktev.jd02_01;
+
+public class Buyer extends Thread implements IBuyer, IUseBacket {
+
+    private int num;
+
+    public Buyer(int num) {
+        super("Покупатель №"+num);
+        this.num = num;
+    }
+
+    @Override
+    public void enterToMarket() {
+        System.out.println(this+" зашёл в магазин");
+    }
+
+    @Override
+    public void goToOut() {
+        System.out.println(this+" вышел из магазина");
+    }
+
+    @Override
+    public void chooseGoods() {
+        System.out.println(this+" вошёл в зал");
+        try {
+            sleep((int)(Math.random()*3000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(this+" выбрал товары");
+    }
+
+    @Override
+    public void takeBacket() {
+    }
+
+    @Override
+    public void backBacket() {
+
+    }
+
+    @Override
+    public void putGoodsToBacket() {
+    }
+
+    @Override
+    public void run() {
+
+        enterToMarket();
+
+        chooseGoods();
+
+        goToOut();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Покупатель №"+num;
+    }
+}
--- src/by/it/loktev/jd02_01/Runner.java
+++ src/by/it/loktev/jd02_01/Runner.java
@@ -2,7 +2,19 @@ package by.it.loktev.jd02_01;
 
 public class Runner {
 
-    public static void main(String[] args) {
+    public static int countBuyers=0;
+    public static int plan=10;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (countBuyers<plan)
+        {
+            Buyer b=new Buyer(++countBuyers);
+            b.start();
+            Thread.sleep((int)(Math.random()*700+300));
+        }
+
+        //System.out.println("Завершено");
 
     }
 
