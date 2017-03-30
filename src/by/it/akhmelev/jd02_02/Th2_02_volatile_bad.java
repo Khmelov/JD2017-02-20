3486ec256466cc3e35873340e2a3d502341f9fcc
Alex <akhmelev@gmail.com>
2017-03-31 00:56:28 +0300

    jd02_02 Threads (sync)

--- src/by/it/akhmelev/jd02_02/Th2_02_volatile_bad.java
+++ src/by/it/akhmelev/jd02_02/Th2_02_volatile_bad.java
@@ -3,7 +3,7 @@ package by.it.akhmelev.jd02_02;
 public class Th2_02_volatile_bad {
 
     //переменная баланса
-    private volatile static Integer balance=0;
+    volatile static Integer balance=0;
 
     //это касса. Просто добавляет в баланс единицу
     static class Cashier extends Thread {
