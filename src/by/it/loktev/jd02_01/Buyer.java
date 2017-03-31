006c268385173db04bb52bdbae09e2d1c785903b
loktevalexey <loktev.alex.74@gmail.com>
2017-03-31 09:55:27 +0300

    after refork

--- src/by/it/loktev/jd02_01/Buyer.java
+++ src/by/it/loktev/jd02_01/Buyer.java
@@ -2,14 +2,12 @@ package by.it.loktev.jd02_01;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class Buyer extends Thread implements IBuyer, IUseBacket {
 
     private int num;
-    public int queueNum;
 
-    public boolean isPensioner;
+    private boolean isPensioner;
     private double timeMul; // коэффициент увеличения времени на все операции
 
     private Map<Good,Double> backet;
@@ -46,13 +44,9 @@ public class Buyer extends Thread implements IBuyer, IUseBacket {
             System.out.println(this+" выбрал товар "+good.getName()+" ценой "+good.getPrice()+" "+quantity+" штук");
         }
         Helper.sleep(Helper.getRandom(400,700)*timeMul);
-        /*
         double backetPrice= getBacketPrice();
-        synchronized (Shop.class) {
-            Shop.totalPrice2 += backetPrice;
-        }
-        */
-        System.out.println(this+" пошёл на кассу");
+        Shop.totalPrice+=backetPrice;
+        System.out.println(this+" пошёл на кассу, оплатил "+backetPrice);
     }
 
     @Override
@@ -78,51 +72,26 @@ public class Buyer extends Thread implements IBuyer, IUseBacket {
     @Override
     public double getBacketPrice() {
         double price=0;
+        /*Set<Good,Double> backet=*/ //backet.entrySet().iterator();
         for ( Map.Entry<Good,Double> goodEntry : backet.entrySet() ){
            price+=goodEntry.getKey().getPrice()*goodEntry.getValue();
         }
         return price;
     }
 
-    public Set<Map.Entry<Good,Double>> getBucket(){
-        return backet.entrySet();
-    }
-
-    @Override
-    public void gotoQueue() {
-        BuyersQueue.add(this);
-        System.out.println(this+" стал в очередь");
-        synchronized (this){
-           try {
-               this.wait();
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
-       System.out.println(this+" завершил обслуживание в очереди");
-    }
-
     @Override
     public void run() {
-        synchronized (Shop.class) {
-            Shop.buyersCount2++;
-        }
+        Shop.buyersCount++;
         enterToMarket();
         takeBacket();
         chooseGoods();
-        gotoQueue();
         backBacket();
         goToOut();
-        synchronized (Shop.class) {
-            Shop.buyersCount2--;
-        }
+        Shop.buyersCount--;
     }
 
     @Override
     public String toString() {
-        if (isPensioner)
-            return "Покуп/ПЕНС №"+num;
-        else
-            return "Покупатель №"+num;
+        return "Покупатель №"+num+(isPensioner?" (пенсионер)":"");
     }
 }
