99faf45fcf9b6c3da50c8f1636d82acf20f0d329
Prigozhanov <vprigozhanov@gmail.com>
2017-04-03 12:34:23 +0300

    jd02_03

--- src/by/it/prigozhanov/jd02_02/Runner.java
+++ src/by/it/prigozhanov/jd02_02/Runner.java
@@ -10,6 +10,7 @@ public class Runner {
     public static void main(String[] args) {
         Buyer buyer;
         Timer timer = new Timer(2, true);
+        timer.setDaemon(true);
         timer.start();
         new Dispatcher().start();
         while (Dispatcher.countBuyers < Dispatcher.plan) {
