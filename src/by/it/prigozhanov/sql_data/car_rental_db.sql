35ed1370c8032138d7c626c50ce6c91ce000c3d0
Prigozhanov <vprigozhanov@gmail.com>
2017-05-01 09:01:04 +0300

    jd03_01 A, B

--- src/by/it/prigozhanov/sql_data/car_rental_db.sql
+++ src/by/it/prigozhanov/sql_data/car_rental_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1:2016
--- Время создания: Апр 28 2017 г., 17:27
+-- Время создания: Май 01 2017 г., 08:00
 -- Версия сервера: 10.1.10-MariaDB
 -- Версия PHP: 5.6.15
 
@@ -23,20 +23,87 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `cars`
+--
+
+CREATE TABLE `cars` (
+  `ID` int(11) NOT NULL,
+  `Mark` varchar(100) NOT NULL,
+  `Model` varchar(100) NOT NULL,
+  `HP` int(11) NOT NULL,
+  `Location` varchar(100) NOT NULL,
+  `Luggage_capacity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `cars`
+--
+
+INSERT INTO `cars` (`ID`, `Mark`, `Model`, `HP`, `Location`, `Luggage_capacity`) VALUES
+(1, 'BMW', 'M3', 600, 'Minsk', 3),
+(2, 'Porsche', 'Panamera', 650, 'Berlin', 2),
+(3, 'Renault', 'Sandero', 110, 'Minsk', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `ID` int(11) NOT NULL,
+  `Role` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`ID`, `Role`) VALUES
+(1, 'Administrator'),
+(2, 'Client'),
+(3, 'Guest'),
+(4, 'Regular');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `users`
 --
 
 CREATE TABLE `users` (
   `ID` int(11) NOT NULL,
-  `Role` varchar(100) NOT NULL,
+  `Passport_data` varchar(100) NOT NULL,
   `Login` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL
+  `Email` varchar(100) NOT NULL,
+  `FK_Role` int(11) NOT NULL DEFAULT '2',
+  `FK_Cars` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`ID`, `Passport_data`, `Login`, `Password`, `Email`, `FK_Role`, `FK_Cars`) VALUES
+(1, 'MP9838582', 'Admin', 'admin', 'admin@gmail.com', 1, NULL),
+(3, 'ME930933', 'Ivanov', '12345', 'Ivanov1983@mail.ru', 2, NULL);
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Индексы таблицы `users`
@@ -49,10 +116,20 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
