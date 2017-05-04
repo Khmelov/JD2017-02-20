bab66d26eb8cfd6ed56de2cd6c84c9f9d1c1aaa3
Prigozhanov <vprigozhanov@gmail.com>
2017-05-04 17:06:07 +0300

    jd03_01 Task A, B, C jd03_02 A, B, C

--- src/by/it/prigozhanov/jd03_03/TestDaoRunner.java
+++ src/by/it/prigozhanov/jd03_03/TestDaoRunner.java
@@ -15,13 +15,29 @@ import java.util.List;
  */
 public class TestDaoRunner {
     public static void main(String[] args) throws SQLException {
+
         DAO dao = DAO.getInstance();
+        System.out.println("GET ALL: -----------------");
         List<User> users = dao.user.getAll("");
         System.out.println(users);
         List<Role> roles = dao.role.getAll("");
         System.out.println(roles);
         List<Car> cars = dao.car.getAll("");
         System.out.println(cars);
+        System.out.println("GET ALL: -----------------\n");
+
+       // System.out.println(dao.user.getAll("INNER JOIN `roles` ON users.FK_Roles=roles.ID"));
+        System.out.println("CRUD: ----------------");
+        User user = new User(0, "TestUser", "TestUser", "TestUser", "TestUser", 0, 0);
+        dao.user.create(user);
+        System.out.println(dao.user.read(user.getId()));
+        dao.user.update(new User(user.getId(), "TestUpdate", "TestUpdate", "TestUpdate", "TestUpdate", 1, 1));
+        System.out.println(dao.user.read(user.getId()));
+        dao.user.delete(user);
+        users = dao.user.getAll("");
+        System.out.println(users);
+        System.out.println("CRUD: ----------------\n");
+
     }
 
 }
