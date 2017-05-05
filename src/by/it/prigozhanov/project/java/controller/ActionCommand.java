c6f79a25f053800b2079c397160a3e1d5794e3cb
Prigozhanov <vprigozhanov@gmail.com>
2017-05-05 12:36:11 +0300

    jd 03(01,02,03) done

---
+++ src/by/it/prigozhanov/project/java/controller/ActionCommand.java
@@ -0,0 +1,12 @@
+package by.it.prigozhanov.project.java.controller;
+
+import com.sun.deploy.net.HttpRequest;
+
+/**
+ * Created by v-omf on 5/5/2017!
+ */
+public interface ActionCommand {
+    String execute(HttpRequest request);
+
+
+}
